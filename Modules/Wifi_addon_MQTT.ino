2887dcc996cc12e0b019df698aaea8b233778838
jakesfcoetz <jakesfcoetz@gmail.com>
2021-05-22 12:05:01 +0200

    restructure

---
+++ Modules/Wifi_addon_MQTT.ino
@@ -0,0 +1,139 @@
+//=========================================================================================
+//========================================= MQTT ==========================================
+//=========================================================================================
+
+//======================================= Requires ========================================
+//----- wifiClient - from ESP_Wifi_Web.ino module
+/*
+  //---MQTT subscription topic declarations (project specific)
+  const char* mqtt_topic_but = "MY_ESP8266_BUT";
+  const char* mqtt_topic_led = "MY_ESP8266_LED";
+*/
+//======================================== Loop {} ========================================
+/*
+  //---MQTT
+  mqtt_check_connection();
+
+  //---Check if the pushbutton is pressed. (LOW on pin GPIO0)
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("Button LOW");
+    mqtt_publish(mqtt_topic_but, "Button pressed!");
+    delay(500); //---delay to stop button from transmitting multiple times
+  }
+  
+*/
+//=================================== create functions ====================================
+/*
+  //---Subscribe to MQTT Topics
+  void mqtt_subscriptions() { //---(content =  project specific)
+    client.subscribe(mqtt_topic_led);
+  }
+
+  
+  //---What to do when MQTT subscription message is received
+  void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+    char myPayload[length]; //---convert byte array to char array
+    Serial.print("Message received [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i=0;i<length;i++) {
+      myPayload[i] = (char)payload[i];
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    
+    if (strcmp(myPayload,"1")==0) {
+      //---turn LED on:
+      ledStatus = LOW;
+      Serial.println("LED ON");
+    } else {
+      //---turn LED off:
+      ledStatus = HIGH;
+      Serial.println("LED OFF");
+    }
+  }
+
+*/
+//=================================== Available functions =================================
+//----- bool mqtt_connected()
+//----- void mqtt_callback(char* topic, byte* payload, unsigned int length)
+//----- bool mqtt_connect()
+//----- bool mqtt_check_connection()
+//----- void mqtt_publish(const char* mqtt_pub_toppic, const char* mqtt_pub_payload)
+//=================================== Test on MQTT Server =================================
+//----- mosquitto_sub -d -u coetzee_mqtt -P coetzeemqtt -t MY_ESP8266_BUT
+//----- mosquitto_pub -d -u coetzee_mqtt -P coetzeemqtt -t MY_ESP8266_LED -m "0"
+//=========================================================================================
+//=========================================================================================
+
+#include <PubSubClient.h> //---MQTT broker communication
+const char* mqtt_server = "192.168.10.3"; //---MQTT broker server IP
+const char* mqtt_username = "coetzee_mqtt"; //---MQTT broker username
+const char* mqtt_password = "coetzeemqtt"; //---MQTT broker password
+const char* mqttClientID = "ESP01"; //---The client id identifies the device.
+
+const unsigned long intervalTime_MQTT = (1000 * 1); //---Interval in mil sec (1000 = 1s)
+unsigned long previousTime_MQTT = 0;
+
+//---Initialise MQTT Client object (IP, port, WiFiClient)
+PubSubClient client(mqtt_server, 1883, wifiClient);
+
+/*
+ * Determine if connected to MQTT Broker
+ * return (Boolean)
+ */
+bool mqtt_connected() {
+  if (client.connected()) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+/*
+ * Connect to MQTT Broker
+ */
+bool mqtt_connect() {
+  Serial.print("Attempting MQTT connection...");
+  if (client.connect(mqttClientID, mqtt_username, mqtt_password)) {
+    Serial.println("Connected");
+    //---Set Callback to listen for incomming publications
+    client.setCallback(mqtt_callback);
+    mqtt_subscriptions();
+    return true;
+  } else {
+    Serial.println("failed");
+    return false;
+  }
+}
+
+/*
+ * Check Connction to MQTT Broker
+ */
+bool mqtt_check_connection() {
+  client.loop(); //---Check incoming MQTT messages
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime_checkMQTT >= intervalTime_checkMQTT) {
+    if (!mqtt_connected()) {
+      mqtt_connect();
+    }
+    previousTime_checkMQTT = currentTime;
+  }
+}
+
+/*
+ * PUBLISH to the MQTT Broker
+ */
+void mqtt_publish(const char* mqtt_pub_toppic, const char* mqtt_pub_payload) {
+  if (client.publish(mqtt_pub_toppic, mqtt_pub_payload)) {
+    Serial.println("Message sent!");
+  }
+  //---If the message failed, try again
+  else {
+    Serial.println("Message failed to send. Reconnecting to MQTT Broker and trying again");
+    mqtt_connect();
+    delay(10);
+    client.publish(mqtt_pub_toppic, mqtt_pub_payload);
+  }
+}
---
+++ Modules/Wifi_addon_TimeKeeper.ino
@@ -0,0 +1,97 @@
+//=========================================================================================
+//===================================== Time Keeper =======================================
+//=========================================================================================
+
+//======================================= Requires ========================================
+//----- wifiClient - from ESP_Wifi_Web.ino module
+//======================================== Loop {} ========================================
+/*
+  //---Update Time
+  timeKeeper();
+*/
+//=================================== create functions ====================================
+/*
+  //---Create or add to existing function
+  void webserver_setup_unit() {
+    myWebServer.on("/time", myWebServerHandle_time);
+ }
+*/
+//=================================== Available functions =================================
+//----- timeKeeper()
+//=========================================================================================
+//=========================================================================================
+
+unsigned long time_Now = 0;
+unsigned long time_Pre = 0;
+int time_s = 0;
+int time_m = 0;
+int time_H = 0;
+
+/*
+ * TimeKeeper
+ */
+void timeKeeper() {
+  time_Now = millis()/1000; // the number of milliseconds that have passed since boot
+  time_s = time_Now - time_Pre;//the number of seconds that have passed since the last time 60 seconds was reached.
+  
+  if (time_s >= 60) {
+    time_Pre = time_Pre + 60;
+    time_m += 1;
+//    Serial.println("Current Time - " + String(time_H) + ":" + String(time_m) + ":" + String(time_s) );
+  }
+  
+  if (time_m >= 60){ 
+    time_m = time_m % 60;
+    time_H += 1;
+  }
+  
+  if (time_H == 24)
+    time_H = 0;
+}
+
+/*
+ * URL /time
+ */
+ void myWebServerHandle_time() {
+  myWebServer.send(200, "text/html", html_Time()); 
+}
+
+/*
+ * Create HTML String
+ * requires (Any value needed to create HTML text)
+ * return (String)
+ */
+String html_Time() {
+  String html_text = html_Head();
+  html_text +="<body>\n";
+  html_text +="<h1>Web Control</h1>\n";
+  html_text +="<h3>Time</h3>\n";
+
+  if (myWebServer.arg("doSetTime") == "1") { //---Check if should Connect
+    int new_time_H = myWebServer.arg("time_H").toInt();
+    int new_time_m = myWebServer.arg("time_m").toInt();
+    if ((new_time_H >= 0) and (new_time_H <= 23) and (new_time_m >= 0) and (new_time_m <= 59)) {
+      time_H = new_time_H;
+      time_m = new_time_m;
+      Serial.print("Time set to: ");
+      Serial.print(time_H);
+      Serial.print(":");
+      Serial.println(time_m);
+    }
+  }
+  
+  html_text +="<form name='formTime' id='formTime' method='POST' action='/time'>\n";
+  html_text +="<input type='text' size=2 name='time_H' id='time_H' value='";
+  html_text += time_H;
+  html_text +="'> : ";
+  html_text +="<input type='text' size=2 name='time_m' id='time_m' value='";
+  html_text += time_m;
+  html_text += "'>";
+  html_text +="<input type='hidden' name='doSetTime' id='doSetTime' value=0>\n";
+  html_text +="<input type='submit' class='button_small' value='Set Time' onclick=document.getElementById('doSetTime').value=1>\n";
+  html_text +="</form><br>\n";
+  html_text +="<a class=\"button_large\" href=\"/\">Home</a>\n";
+  html_text +="</body>\n";
+  html_text +="</html>\n";
+  return html_text;
+}
